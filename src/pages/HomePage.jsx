70d60134618d9342570b18203adc4565735ee349
Dante Harasz <danteharasz@gmail.com>
2020-04-26 19:50:12 -0400

    Cardinal direction function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
 
-// API key for openweathermap.org
+// data for fetch url
 const key = "770d3167b3eba3b1c6578ba7c1153c3b"
 const url = "https://api.openweathermap.org/data/2.5/onecall"
 const pitts = {
   lat : 40.44,
   lon : -79.99
 }
+// data for dynamic background switch
 const indicate = {
   sun: "https://cdn.pixabay.com/photo/2013/07/13/10/23/sun-157126_1280.png",
   rain: "https://cdn.pixabay.com/photo/2018/05/31/13/13/rain-3443977_1280.jpg",
@@ -20,7 +21,7 @@ const HomePage = () => {
   const  [errors, setErrors] =  useState(false)
   const  [weather,setWeather ]= useState({})
 
-  async function getWeather() {
+  const getWeather = async () => {
     const get = await fetch(`${url}?lat=${pitts.lat}&lon=${pitts.lon}&appid=${key}`);
     get
       .json()
@@ -30,6 +31,7 @@ const HomePage = () => {
           currentDescription: resp.current.weather[0].main,
           currentWindSpeed: resp.current.wind_speed + 'mph',
           currentWindDir: resp.current.wind_deg + '°',
+          currentWindDeg: resp.current.wind_deg,
         })
         console.log(resp)
       })
@@ -38,13 +40,33 @@ const HomePage = () => {
         console.log(errors)
       });
   }
-  function convert(k) {
+  const convert = (k) => {
     k += -273.15
     let f = k * 9/5 
     f += 32
     return Math.round(f) + '°F'
   }
-  function photo() {
+  const direction = () => {
+    let deg = weather.currentWindDeg
+    if (338 < deg < 24) {
+      return 'North'
+    } else if (295 < deg < 337) {
+      return 'NorthWest'
+    } else if (248 < deg < 294) {
+      return 'West'
+    } else if (203 < deg < 247) {
+      return 'SouthWest'
+    } else if (157 < deg < 202) {
+      return 'South'
+    } else if (112 < deg < 156) {
+      return 'SouthEast'
+    } else if (69 < deg < 111) {
+      return 'East'
+    } else if (25 < deg < 68) {
+      return 'NorthEast'
+    }
+  }
+  const photo = () => {
     let itIs = weather.currentDescription
     if ( itIs === 'Rain') {
       return `url(${indicate.rain})`
@@ -60,13 +82,12 @@ const HomePage = () => {
       return `url(${indicate.sun})`
     }
   }
+
   useEffect(() => {
     getWeather()
   },[])
   return <div className="center-flex" 
   style={{backgroundImage: photo()}}>
-    {/* test log area
-    {JSON.stringify({weather})} */}
     <section>
     <h1>
       Current Weather In Pittsburgh
@@ -75,15 +96,15 @@ const HomePage = () => {
         {weather.currentTemp}
         </h2>
         <h3>
-          conditions: {weather.currentDescription}
+          Conditions: {weather.currentDescription}
         </h3>
         <h4>
-          wind speed: {weather.currentWindSpeed}
+          Wind speed: {weather.currentWindSpeed}
         </h4>
         <h4>
-          wind direction: {weather.currentWindDir}
+          Wind direction: {direction()}-{weather.currentWindDir}
         </h4>
-        <img id="indicator" alt="#" src={weather.currentDescription === "Rain" ? indicate.rain : indicate.sun} />
+        <img id="indicator" alt="#" src={photo().slice(4,-1)} />
         </section>
     </div>
 }
