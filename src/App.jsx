29c3557c0cfcf27ad76d2e85795158c0d9574e19
Dante Harasz <danteharasz@gmail.com>
2020-04-26 18:39:19 -0400

    mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <Router>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Go Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="*" component={NotFound}></Route>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,37 +2,73 @@ import React, {useState, useEffect} from 'react'
 
 // API key for openweathermap.org
 const key = "770d3167b3eba3b1c6578ba7c1153c3b"
+const url = "https://api.openweathermap.org/data/2.5/onecall"
 const pitts = {
   lat : 40.44,
   lon : -79.99
 }
+const indicate = {
+  sun: "https://cdn.pixabay.com/photo/2013/07/13/10/23/sun-157126_1280.png",
+  rain: "https://cdn.pixabay.com/photo/2018/05/31/13/13/rain-3443977_1280.jpg"
+}
+
 const HomePage = () => {
-  const  [hasError, setErrors] =  useState(false)
+  const  [, setErrors] =  useState(false)
   const  [weather,setWeather ]= useState({})
-  async function fetchData() {
-    const get = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${pitts.lat}&lon=${pitts.lon}&appid=${key}`);
+
+  async function getWeather() {
+    const get = await fetch(`${url}?lat=${pitts.lat}&lon=${pitts.lon}&appid=${key}`);
     get
       .json()
       .then(resp => {
-        setWeather(resp)
+        setWeather({
+          currentTemp: convert(resp.current.temp),
+          currentDescription: resp.current.weather[0].main,
+          currentWindSpeed: resp.current.wind_speed + 'mph',
+          currentWindDir: resp.current.wind_deg + '°',
+        })
         console.log(resp)
       })
       .catch(err => setErrors(err));
   }
-  function convert(kelvin) {
-    kelvin += -273.15
-    let f = kelvin * 9/5 
+  function convert(k) {
+    k += -273.15
+    let f = k * 9/5 
     f += 32
-    return f
+    return Math.round(f) + '°F'
+  }
+  function photo() {
+    if (weather.currentDescription === 'Rain') {
+      return `url(${indicate.rain})`
+    } else {
+      return `url(${indicate.sun})`
+    }
   }
   useEffect(() => {
-    fetchData()
+    getWeather()
   },[])
-  return <div>
-
-    <button>Get Weather</button>
-    <h4>Current Temp</h4>
-
+  return <div className="center-flex" 
+  style={{backgroundImage: photo()}}>
+    {/* test log area
+    {JSON.stringify({weather})} */}
+    <section>
+    <h1>
+      Current Weather In Pittsburgh
+      </h1>
+      <h2>
+        {weather.currentTemp}
+        </h2>
+        <h3>
+          conditions: {weather.currentDescription}
+        </h3>
+        <h4>
+          wind speed: {weather.currentWindSpeed}
+        </h4>
+        <h4>
+          wind direction: {weather.currentWindDir}
+        </h4>
+        <img id="indicator" alt="#" src={weather.currentDescription === "Rain" ? indicate.rain : indicate.sun} />
+        </section>
     </div>
 }
 export default HomePage
